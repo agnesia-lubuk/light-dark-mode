0328577f23149d46107e14cf7ef335c83c6f8b92
agnesia-lubuk <agnesia.lubuk@gmail.com>
2022-09-23 00:03:57 +0700

    fixing bugs in images /

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ const LIGHT_THEME = 'light'
 
 // Dark or Light Images
 const imageMode = (color) => {
-  image1.src = `/img/undraw_proud_coder_${color}.svg`
-  image2.src = `/img/undraw_feeling_proud_${color}.svg`
-  image3.src = `/img/undraw_conceptual_idea_${color}.svg`
+  image1.src = `img/undraw_proud_coder_${color}.svg`
+  image2.src = `img/undraw_feeling_proud_${color}.svg`
+  image3.src = `img/undraw_conceptual_idea_${color}.svg`
 }
 
 // Helper Function for Modes
